fd37b7865dd106d7c8f28eaf8dae595bbdbae7c6
Szymon <szymon.kowalewski@gmail.com>
2020-08-27 16:42:39 +0200

    Start filling process_folder method

--- leaf_extractor.py
+++ leaf_extractor.py
@@ -3,7 +3,7 @@ import cv2
 
 class InstanceExtractor():
     def __init__(self):
-        pass
+        self.instance_id = 0
 
     def extract_instances(self, sheet_image):
         pass
@@ -11,5 +11,21 @@ class InstanceExtractor():
     def pad_instance(self, instance_image):
         pass
 
-    def process_folder(self, input_path, output_path):
+    def save_instance(self, image):
         pass
+
+    def process_folder(self, input_path, output_path):
+        self.instance_id = 0
+        self.input_path = input_path
+        self.output_path = output_path
+        ## TODO List sheets in input folder
+        sheet_paths = []
+        for sheet_path in sheet_paths:
+            # TODO Load image
+            # Extract instances
+            instances = self.extract_instances(sheet)
+            for instance in instances:
+                # Pad to equal size
+                padded_instance = self.pad_instance(instance)
+                # Save instance
+                self.save_instance(padded_instance)
