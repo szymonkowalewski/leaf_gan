a12e0ebf3ba7e481a3825b2b902a112f2b9a5364
Szymon <szymon.kowalewski@gmail.com>
2020-08-27 16:11:10 +0200

    placeholders for instance extractor

--- leaf_extractor.py
+++ leaf_extractor.py
@@ -1,2 +1,15 @@
 # Script for extracting single leaves form a sheet with multiple samples
 import cv2
+
+class InstanceExtractor():
+    def __init__(self):
+        pass
+
+    def extract_instances(self, sheet_image):
+        pass
+
+    def pad_instance(self, instance_image):
+        pass
+
+    def process_folder(self, input_path, output_path):
+        pass
